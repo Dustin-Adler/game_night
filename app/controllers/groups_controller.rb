38af8de82d49780f298dbcea0e8d56a77fb04ae4
Dustin <dustin.adler@yahoo.com>
2022-02-07 11:49:48 -0600

    group creation, and edit + basic views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,59 +1,42 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[ show edit update destroy ]
 
-  # GET /groups or /groups.json
-  def index
-    @groups = Group.all
-  end
-
-  # GET /groups/1 or /groups/1.json
   def show
   end
 
-  # GET /groups/new
-  def new
-    @group = Group.new
-  end
-
-  # GET /groups/1/edit
   def edit
   end
 
-  # POST /groups or /groups.json
   def create
     @group = Group.new(group_params)
+    @group.admin_id = current_user.id
 
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to group_url(@group), notice: "Group was successfully created." }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    if @group.save
+      redirect_to dashboards_path, notice: "Group was successfully created."
+    else
+      redirect_to dashboards_path, alert: @group.errors.full_messages.first
     end
   end
 
-  # PATCH/PUT /groups/1 or /groups/1.json
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: "Group was successfully updated." }
+        format.html { redirect_to dashboards_url, notice: "Group was successfully updated." }
         format.json { render :show, status: :ok, location: @group }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
+        format.html { redirect_to dashboards_url, alert: @group.errors.full_messages.first }
+        format.json { render json: @group.errors }
       end
     end
   end
 
   # DELETE /groups/1 or /groups/1.json
   def destroy
-    @group.destroy
 
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
-      format.json { head :no_content }
+    if @group.destroy
+      redirect_to dashboards_path, notice: "group was successfully deleted."
+    else
+      redirect_to dashboards_path, alert: @group.errors.full_messages
     end
   end
 
@@ -65,6 +48,6 @@ class GroupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def group_params
-      params.require(:group).permit(:admin_id, :group_name, :description)
+      params.require(:group).permit(:group_name, :description)
     end
 end
